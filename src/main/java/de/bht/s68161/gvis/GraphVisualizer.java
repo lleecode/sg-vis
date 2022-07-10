a8ed517a406d805ca0f8c9214bfa2a9e6c073239
Lucas Lee <lucas.lee@dkbcodefactory.com>
2022-07-10 18:01:39 +0200

    Add randomly colored edges

--- src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
+++ src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
@@ -1,5 +1,6 @@
 package de.bht.s68161.gvis;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.implementations.SingleGraph;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,10 +12,11 @@ public class GraphVisualizer {
         displayRandomGraph(10);
     }
 
-
     private static void displayRandomGraph(int size) throws InterruptedException {
 
         SingleGraph graph = new SingleGraph("0", false, false);
+
+        graph.setAttribute("ui.stylesheet", "url('file:./src/main/resources/graph.css')");
 
         graph.display();
 
@@ -22,15 +24,21 @@ public class GraphVisualizer {
             Thread.sleep(200);
             graph.addNode("" + i);
         }
-        for (int i = 0; i < ThreadLocalRandom.current().nextInt(size / 2, size); i++) {
+        for (int i = 0; i < size; i++) {
             Thread.sleep(200);
-            graph.addEdge(
+            Edge e = graph.addEdge(
                     i + "",
                     ThreadLocalRandom.current().nextInt(0, size) + "",
                     ThreadLocalRandom.current().nextInt(0, size) + ""
             );
+            if (e != null) {
+                if (i % 2 == 0) e.setAttribute("ui.class", "one");
+                else e.setAttribute("ui.class", "two");
+            }
+        }
+        for (int i = 0; i < graph.getEdgeCount(); i++) {
+            Thread.sleep(200);
+            graph.getEdge(i).setAttribute("ui.fill-color", "red");
         }
     }
-
-
 }
