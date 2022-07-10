da5c08fc582c3fa70a00b9c8fc6be2ed0fe42716
Lucas Lee <lucas.lee@dkbcodefactory.com>
2022-07-10 17:16:06 +0200

    Refactor

--- src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
+++ src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
@@ -1,23 +1,23 @@
 package de.bht.s68161.gvis;
 
 import org.graphstream.graph.implementations.SingleGraph;
-import org.graphstream.ui.view.Viewer;
 
 import java.util.concurrent.ThreadLocalRandom;
-
 
 public class GraphVisualizer {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("org.graphstream.ui", "swing");
-        buildRandomGraph(10);
+        displayRandomGraph(10);
     }
 
 
-    private static SingleGraph buildRandomGraph(int size) throws InterruptedException {
+    private static void displayRandomGraph(int size) throws InterruptedException {
 
         SingleGraph graph = new SingleGraph("0", false, false);
-        Viewer v = graph.display();
+
+        graph.display();
+
         for (int i = 0; i < size; i++) {
             Thread.sleep(200);
             graph.addNode("" + i);
@@ -30,7 +30,7 @@ public class GraphVisualizer {
                     ThreadLocalRandom.current().nextInt(0, size) + ""
             );
         }
-
-        return graph;
     }
+
+
 }
