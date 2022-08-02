702db711491008afbeffe0ccb1907e909b15593c
Lucas Lee <lucas.lee@dkbcodefactory.com>
2022-08-02 22:17:47 +0200

    Refine example

--- src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
+++ src/main/java/de/bht/s68161/gvis/GraphVisualizer.java
@@ -13,47 +13,16 @@ import java.util.stream.Collectors;
 
 public class GraphVisualizer {
 
-    private static final int SLEEP_DURATION = 2000;
+    private static final int SLEEP_DURATION = 1000;
 
-    public static void main(String[] args) throws InterruptedException {
-        // initExampleGraph();
+    public static void main(String[] args) {
         initExampleKruskal();
     }
 
-    private static void initExampleGraph() throws InterruptedException {
-        VisualizedGraph vGraph = new VisualizedGraph();
-        VisualizedMST vMST = new VisualizedMST(vGraph);
-
-        Arrays.stream(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I"}).forEach(vGraph::addNode);
-
-        vGraph.addEdge("eh", "E", "H", 6);
-        vGraph.addEdge("ga", "F", "A", 7);
-        vGraph.addEdge("ch", "C", "H", 9);
-        WeightedEdge ab = vGraph.addEdge("ab", "A", "B", 3);
-        WeightedEdge ae = vGraph.addEdge("ae", "A", "E", 4);
-        WeightedEdge bf = vGraph.addEdge("bf", "B", "F", 2);
-        WeightedEdge di = vGraph.addEdge("di", "D", "I", 5);
-        WeightedEdge fi = vGraph.addEdge("fi", "F", "I", 4);
-        WeightedEdge hi = vGraph.addEdge("hi", "H", "I", 3);
-        WeightedEdge gf = vGraph.addEdge("gf", "G", "F", 1);
-        WeightedEdge ec = vGraph.addEdge("ec", "E", "C", 8);
-
-        Thread.sleep(SLEEP_DURATION);
-
-        vMST.addEdgeToMST(ab);
-        vMST.addEdgeToMST(ae);
-        vMST.addEdgeToMST(bf);
-        vMST.addEdgeToMST(di);
-        vMST.addEdgeToMST(fi);
-        vMST.addEdgeToMST(hi);
-        vMST.addEdgeToMST(gf);
-        vMST.addEdgeToMST(ec);
-    }
-
-    private static void initExampleKruskal() throws InterruptedException {
+    private static void initExampleKruskal() {
 
         VisualizedGraph vGraph = new VisualizedGraph();
-        VisualizedMST vMST = new VisualizedMST(vGraph, 1000);
+        VisualizedMST vMST = new VisualizedMST(vGraph, SLEEP_DURATION);
 
         Arrays.stream(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I"}).forEach(vGraph::addNode);
 
@@ -72,7 +41,7 @@ public class GraphVisualizer {
         kruskal(vGraph, vMST);
     }
 
-    private static HashSet<WeightedEdge> kruskal(VisualizedGraph graph, VisualizedMST mst) {
+    private static void kruskal(VisualizedGraph graph, VisualizedMST mst) {
         HashSet<HashSet<Node>> nodeClusters = new HashSet<>();
         graph.getNodes().forEach(node -> {
             HashSet<Node> cluster = new HashSet<>();
@@ -93,7 +62,7 @@ public class GraphVisualizer {
             HashSet<Node> clusterA = findContainingCluster(nodeClusters, nodeA);
             HashSet<Node> clusterB = findContainingCluster(nodeClusters, nodeB);
 
-            if(clusterA != clusterB) {
+            if (clusterA != clusterB) {
                 HashSet<Node> merged = new HashSet<>();
                 merged.addAll(clusterA);
                 merged.addAll(clusterB);
@@ -103,8 +72,6 @@ public class GraphVisualizer {
                 mst.addEdgeToMST(edge);
             }
         }
-
-        return mst.getEdges();
     }
 
     private static HashSet<Node> findContainingCluster(HashSet<HashSet<Node>> nodeClusters, Node node) {
