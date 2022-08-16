1eed09e53642c197d9d8d67954dc9b24c0f55dbe
Lucas Lee <lucas.lee@dkbcodefactory.com>
2022-08-16 22:08:03 +0200

    Add methods to add elements to graph

--- src/main/java/de/bht/lucaslee/gvis/graph/WeightedGraph.java
+++ src/main/java/de/bht/lucaslee/gvis/graph/WeightedGraph.java
@@ -23,6 +23,12 @@ public class WeightedGraph {
         return Node.from(node);
     }
 
+    public Node addNode(Node node) {
+        org.graphstream.graph.Node node_ = graph.addNode(node.getName());
+        node_.setAttribute(Attributes.LABEL, node_.getId());
+        return node;
+    }
+
     public Node removeNode(String name) throws ElementNotFoundException {
         return Node.from(graph.removeNode(name));
     }
@@ -40,6 +46,12 @@ public class WeightedGraph {
         Edge e = graph.addEdge(name, nodeA, nodeB);
         e.setAttribute(Attributes.LABEL, weight);
         return new WeightedEdge(e.getId(), Node.from(e.getNode0()), Node.from(e.getNode1()), weight);
+    }
+
+    public WeightedEdge addEdge(WeightedEdge edge) {
+        Edge edge_ = graph.addEdge(edge.getName(), edge.getNodeA().getName(), edge.getNodeB().getName());
+        edge_.setAttribute(Attributes.LABEL, edge.getWeight());
+        return edge;
     }
 
     public WeightedEdge removeEdge(WeightedEdge edge) throws ElementNotFoundException {
