0c32d795a519e8017c013ab462a7c329955c93aa
Lucas Lee <lucas.lee@dkbcodefactory.com>
2022-09-05 20:16:33 +0200

    Add eq test for nodes

--- src/test/java/de/bht/lucas/lee/gvis/NodeTest.java
+++ src/test/java/de/bht/lucas/lee/gvis/NodeTest.java
@@ -4,6 +4,7 @@ import de.bht.lucaslee.gvis.Node;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class NodeTest {
 
@@ -12,6 +13,15 @@ public class NodeTest {
         String name = "test name node";
         Node n = new Node(name);
         assertEquals(name, n.getName());
+    }
+
+    @Test
+    public void testEquality() {
+        Node n0 = new Node("0");
+        Node n0copy = new Node("0");
+        Node n1 = new Node("1");
+        assertEquals(n0, n0copy);
+        assertNotEquals(n0, n1);
     }
 
 }
